685706876ed70d20a05567c79a4c6764ce42ee75
Vilte N <viltenor@gmail.com>
2022-09-09 12:14:59 +0100

    pull request 1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -3,5 +3,6 @@ public class Runner {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("we are working on develop");
+        System.out.println("we are going to make a pull request");
     }
 }
